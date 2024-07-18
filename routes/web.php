906467c9348f9f69291efa8eb9fa6e6e667d44ef
Valeria <05valarist@gmail.com>
2024-07-18 11:56:23 -0500

    manejo de web.php
    Como manejar las URL en laravel

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "welcome to homepage";
+});
+
+Route::get("/posts", function(){
+    return "Aquí encontrará todos los posts del blog";
+});
+
+/*
+Route::get("/posts/{post}", function($post){
+    return "Estamos en el post {$post}";
+});
+
+Route::get("/posts/{post}/{category}", function($post, $category){
+    return "Estamos en el post {$post} que cuenta con la categoría {$category}";
+});*/
+
+Route::get('posts/{post}/{category?}', function ($post, $category = null) {
+    if($category == null){
+        return "Estamos en el post {$post}";
+    } else {
+        return "Estamos en el post {$post} que cuenta con la categoría {$category}";
+    }
 });
