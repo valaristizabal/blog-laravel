43947fbec9447c319f5f770b22bf68498633b1ac
Valeria <05valarist@gmail.com>
2024-07-23 10:24:45 -0500

    crud pt1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PlantillaController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, "mostrarVentanaPrincipal"]);
@@ -20,3 +21,49 @@ Route::get("/posts/{post}/{category}", function($post, $category){
 Route::get('posts/{post}/{category?}', [HomeController::class, "mostrarCategoriaExistente"]);
 
 Route::get("/plantillas", [PlantillaController::class, "mostrarVentanaPrincipal"]);
+
+Route::get("prueba", function(){
+
+    /*CREAR POST
+
+    $post = new Post;
+    $post->tittle = "titulo de prueba 1";
+    $post->content = "contenido de prueba 1";
+    $post->categorie ="categoria de prueba 1";
+
+    $post->save();
+
+    return $post;*/
+
+    /*BUSCAR POST
+
+    $post = Post::find(1);
+    $post = Post::where("tittle", "titulo de prueba 1")->first();
+    return $post;
+    */
+
+    /*ACTUALIZAR POST
+
+    $post = Post::find(1);
+    $post->categorie = "categoría verdadera";
+    $post-> save();
+    return $post;
+    */
+    
+    /*MOSTRAR VARIOS REGISTROS*/
+
+    //TODOS LOS REGISTROS SIN FILTRO
+    //$posts = Post::all();     
+    //$posts = Post::get();
+
+    //REGISTROS FILTRADOS
+    //$posts =Post::where("id", ">=", "2")->get();
+
+    //ORDENAR REGISTROS
+    //$posts = Post::orderBy("id","desc")->get();
+    
+    //SELECCIONAR REGISTROS ESPECIFICOS
+    $posts = Post::orderBy("id","desc")->select("id", "tittle", "categorie")->get();
+    return $posts;
+    
+});
